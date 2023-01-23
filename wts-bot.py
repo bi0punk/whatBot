229a878c616e4ca24d981f0dce0155d37153a958
sysbot <systecinfo_cl@criptext.com>
2023-01-23 13:29:27 -0300

    order

--- wts-bot.py
+++ wts-bot.py
@@ -6,8 +6,6 @@ from datetime import time, date
 import pandas as pd
 import urllib.request
 
-
-
 def fecha_efemerides():
 
     today = date.today()
@@ -16,7 +14,6 @@ def fecha_efemerides():
     print("dia:", today.day)
     link = 'https://es.wikipedia.org/wiki/'+ str(today.day) + '_de_enero'
     print(link)
-
 
 def trends_scrapper():
 
@@ -45,3 +42,18 @@ envia_mensaje()
 
 
 
+""" # Same as above but Closes the Tab in 2 Seconds after Sending the Message
+    pywhatkit.sendwhatmsg("+910123456789", "Hi", 13, 30, 15, True, 2)
+    # Send an Image to a Group with the Caption as Hello
+    pywhatkit.sendwhats_image("AB123CDEFGHijklmn", "Images/Hello.png", "Hello")
+    # Send an Image to a Contact with the no Caption
+    pywhatkit.sendwhats_image("+910123456789", "Images/Hello.png")
+    # Send a WhatsApp Message to a Group at 12:00 AM
+    pywhatkit.sendwhatmsg_to_group("AB123CDEFGHijklmn", "Hey All!", 0, 0)
+    # Send a WhatsApp Message to a Group instantly
+    pywhatkit.sendwhatmsg_to_group_instantly("AB123CDEFGHijklmn", "Hey All!")
+    # Play a Video on YouTube
+    pywhatkit.playonyt("PyWhatKit") """
+
+
+""" envia_mensaje(trends_texto) """
