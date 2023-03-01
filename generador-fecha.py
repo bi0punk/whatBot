a3be0a8632e6e70a8c952a6237d4c7a3eae48697
sysbot <systecinfo_cl@criptext.com>
2023-02-28 22:38:41 -0300

    add new features solutions

--- generador-fecha.py
+++ generador-fecha.py
@@ -7,45 +7,6 @@ import requests
 import time
 import csv
 
-
-# Python program to display calendar of
-# given month of the year
-
-
-def calendarios():
-    
-    dias = list(range(1, 32))
-
-    print(dias)
-
-def generador_fechas():
-    inicio = datetime.date(2020,1,1)
-    periods = 5
-    daterange = []
-    for day in range(periods):
-        date = (inicio + datetime.timedelta(days = day)).isoformat()
-        daterange.append(date)
-    print(daterange[0])
-
-    for i in daterange:
-        print(i[0:4])
-    """ fecha_data = datetime.datetime.now()
-    mes = (fecha_data.strftime("%m"))
-    dia = (fecha_data.strftime("%d")) """
-    fecha_data= 2020
-    mes = 1
-    dia = 1
-    zzero = '0'
-    
-    global link_sismos2
-    link_sismos2 = f'https://www.sismologia.cl/sismicidad/catalogo/2020/01/202001{(zzero)}{(dia)}.html'
-    #link_sismos2 = f'https://www.sismologia.cl/sismicidad/catalogo/{(fecha_data)}/{(mes)}/{(fecha_data.year)}{(mes)}{(dia)}.html'
-
-    print(link_sismos2)
-    return link_sismos2
-
-generador_fechas()
-
 def timer():
     while True:
         response= requests.get(link_sismos2)   
@@ -69,9 +30,44 @@ def timer():
         print(fecha_data)
         time.sleep(600)   # n segundos.
         
+
+
+def generador_fechas():
+    inicio = datetime.date(2020,1,1)
+    periods = 5
+    daterange = []
+    for day in range(periods):
+        date = (inicio + datetime.timedelta(days = day)).isoformat()
+        daterange.append(date)
+    print(daterange[0])
+
+    for i in daterange:
+        print(i[0:4])
+    """ fecha_data = datetime.datetime.now()
+    mes = (fecha_data.strftime("%m"))
+    dia = (fecha_data.strftime("%d")) """
+    fecha_data= 2020
+    mes = 1
+    dia = 1
+    zzero = '0'
+    
+    global link_sismos2
+    link_sismos2 = f'https://www.sismologia.cl/sismicidad/catalogo/2020/01/202001{(zzero)}{(dia)}.html'
+
+
+
+    #link_sismos2 = f'https://www.sismologia.cl/sismicidad/catalogo/{(fecha_data)}/{(mes)}/{(fecha_data.year)}{(mes)}{(dia)}.html'
+
+    dia = dia + 1
+    print(link_sismos2)
+    return link_sismos2
+
+generador_fechas()
+
+
+
 # Iniciar la ejecución en segundo plano.
 t = threading.Thread(target=timer)
 t.start()
 
 
-calendarios()
